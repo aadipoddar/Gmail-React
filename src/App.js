e82e70d5338055a0cab0f025d3b64e05f849e128
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-18 21:32:27 +0530

    Send Mail Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 import Header from './Header'
 import Sidebar from './Sidebar'
-import EmailList from './EmailList'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Mail from './Mail'
+import EmailList from './EmailList'
+import SendMail from './SendMail'
+import { selectSendMessageIsOpen } from './features/mailSlice'
+import { useSelector } from 'react-redux'
 
 function App() {
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen)
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Header />
-        <div className="app_body">
+        <div className='app_body'>
           <Sidebar />
           <Switch>
-            <Route path="/mail">
+            <Route path='/mail'>
               <Mail />
             </Route>
-            <Route path="/">
+            <Route path='/'>
               <EmailList />
             </Route>
           </Switch>
         </div>
+        {sendMessageIsOpen && <SendMail />}
       </div>
-    </Router >
+    </Router>
   )
 }
 
