37b589c8b78580a01347ec9b4872c63dfac0dcf1
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-18 21:20:40 +0530

    Email List and Rows

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 import Header from './Header'
 import Sidebar from './Sidebar'
+import EmailList from './EmailList'
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="app_body">
+          <Sidebar />
+          <Switch>
+            <Route path="/mail">
+              {/* <Mail /> */}
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router >
   )
 }
 
